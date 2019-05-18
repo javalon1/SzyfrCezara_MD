908511fe1f1a3e89c3044dfec41bc68e52644d00
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-18 12:31:04 +0100

    blank

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,16 @@
+package com.sda.algorytmy;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+
+    }
+}
+
+// Wzor na szyfrowanie   C = (n + k) mod 26
+// Wzor na de - szyfrowanie   C = (n -k) mod 26
