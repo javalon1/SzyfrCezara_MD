719ddfa976d7cca52aaf2ff9d0907335cace2392
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-18 12:53:30 +0100

    Szyfrowanie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,10 +1,37 @@
 package com.sda.algorytmy;
 
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int kluczSZ = 3;
+        final int kluczDESZ = -3;
+        int C;
+
+        System.out.println("Podaj tekst do zaszyfrowania:");
+        String txtSzyfrowany = scanner.nextLine();
+        char[] txtSz = txtSzyfrowany.toCharArray();
+
+        for(int i = 0; i < txtSz.length; i++){
+            int n = txtSz[i];
+
+            //System.out.println(txtSz[i]);
+            //System.out.println(n);
+
+            C = (n + kluczSZ) % 26;
+            //System.out.println(C);
+            txtSz[i] = (char) C;
+            System.out.println(txtSz[i]);
+        }
+
+        //C = (n + k) mod 26
+        // k jest kluczem szyfrowania
+        // n jest numerem litery, którą szyfrujemy
+        // C jest numerem litery po zaszyfrowaniu
 
 
 
@@ -13,4 +40,4 @@ public class App
 }
 
 // Wzor na szyfrowanie   C = (n + k) mod 26
-// Wzor na de - szyfrowanie   C = (n -k) mod 26
+// Wzor na de - szyfrowanie   C = (n - k) mod 26
